027f2d0be71b55dbc3e04c7a475a0565203f334f
Bruma Sebastian <sebastianbruma24@gmail.com>
2025-03-15 16:48:25 +0200

    add a footer and make the navbar header better

--- webapp-rim-previewer/src/app/app.component.ts
+++ webapp-rim-previewer/src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../components/header/header.component';
+import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'webapp-rim-previewer';
+  isFixed = false;
+  showHeader = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (window.scrollY > 50) {
+      if (!this.isFixed) {
+        this.isFixed = true;
+        setTimeout(() => (this.showHeader = true), 50); // Small delay for smooth transition
+      }
+    } else {
+      this.showHeader = false;
+      setTimeout(() => (this.isFixed = false), 300); // Wait for animation before removing fixed
+    }
+  }
 }
